af662336ffe48be9b06fffc8615bbf962efe7af8
bruindav <davedebruin123@gmail.com>
2026-07-12 22:22:48 +0200

    Update Firebase configuration with project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Vul hier je eigen Firebase-projectgegevens in.
 // Te vinden in de Firebase Console → Project settings → Your apps → SDK setup and configuration.
 export const firebaseConfig = {
-  apiKey: "VUL_IN",
-  authDomain: "VUL_IN.firebaseapp.com",
-  projectId: "VUL_IN",
-  storageBucket: "VUL_IN.appspot.com",
-  messagingSenderId: "VUL_IN",
-  appId: "VUL_IN"
+  apiKey: "AIzaSyBTR21Bu1jSRsA8lRnLTlQkrAIZZEcCF3c",
+  authDomain: "mijntuinplanten-be786.firebaseapp.com",
+  projectId: "mijntuinplanten-be786",
+  storageBucket: "mijntuinplanten-be786.firebasestorage.app",
+  messagingSenderId: "943910208291",
+  appId: "1:943910208291:web:64101ca68a3c304531c296",
+  measurementId: "G-FDQ28J5KLL"
 };
